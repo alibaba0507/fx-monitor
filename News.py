a6ad4251d714361eda181a4b5a3670197684d3df
alibaba0507 <alibaba0507@devtable.com>
2013-10-18 18:41:50 +0000

    Commit

---
+++ News.py
@@ -0,0 +1,35 @@
+from google.appengine.ext import ndb
+from google.appengine.api import memcache
+from google.appengine.api import urlfetch
+import urllib
+import logging
+import json
+
+
+#db table for pattern
+class DbNews(ndb.Model):
+  pair = ndb.StringProperty()
+  pr = ndb.StringProperty()
+  alexa = ndb.StringProperty()
+  positive = ndb.IntegerProperty()
+  negative = ndb.IntegerProperty()
+  raatio = ndb.IntegerProperty()
+  
+
+def search(q,pair):
+  #if (!type || type == '')
+  type = 'all+types';
+  url = 'http://pipes.yahoo.com/pipes/pipe.run?_id=16f66113a9aa9c39dd93df8806949159&_render=json&filterinput=' + type + '&maininput=' + q 
+  urlfetch.set_default_fetch_deadline(45)
+  result = urlfetch.fetch(url)
+  if result.status_code == 200:
+    resJSON = json.loads(result.content)
+    items = resJSON.value.items
+    
+    for i in items:
+      link = i.link
+      title = i.title
+    
+    
+    
+    
